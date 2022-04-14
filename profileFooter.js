f00e5c3077ef813c82d412d8809697018fc4bdf9
Nileema Shah <nileemashah@yahoo.com>
2022-04-14 18:07:09 -0400

    Update profileFooter.js

--- profileFooter.js
+++ profileFooter.js
@@ -67,8 +67,8 @@ class myFooter extends HTMLElement {
         
       
       <div id="badges">
-       
-        <a href="https://www.linkedin.com/in/nileema-shah-012501230/recent-activity/shares/">
+        <!-- <a href="https://www.linkedin.com/in/nileema-shah-012501230/recent-activity/shares/"> -->
+        <a href="https://www.linkedin.com/in/nileema-shah-012501230/">
           <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&
                     logoColor=white" alt="LinkedIn Badge" />
         </a>
